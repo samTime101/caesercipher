01acb9c596ea1dda6a923eaade44d557e2e0ad29
samTime101 <srijanregmi44@gmail.com>
2025-01-31 14:29:55 +0545

    more test cases

--- testcases.py
+++ testcases.py
@@ -1,17 +1,38 @@
 import pytest
-from caesercipher import encrypt, decrypt,enter_message
+from caesercipher import encrypt, decrypt, enter_message
 from unittest.mock import patch
-from caesercipher import enter_message
-
-def test_encrypt():
-    assert encrypt('HELLO', 3) == 'KHOOR'
-    assert encrypt('WORLD', 5) == 'BTWQI'
-
-def test_decrypt():
-    assert decrypt('KHOOR', 3) == 'HELLO'
-    assert decrypt('BTWQI', 5) == 'WORLD'
 
 def test_encrypt_with_input_mock():
     with patch('builtins.input', side_effect=['e', 'HELLO', '3']):
         mode, message, shift = enter_message() 
         assert encrypt(message, shift) == 'KHOOR'
+        
+    with patch('builtins.input', side_effect=['e', 'WORLD', '5']):
+        mode, message, shift = enter_message() 
+        assert encrypt(message, shift) == 'BTWQI'
+    
+    with patch('builtins.input', side_effect=['e', 'HELLO WORLD', '4']):
+        mode, message, shift = enter_message() 
+        assert encrypt(message, shift) == 'LIPPS ASVPH'
+
+    with patch('builtins.input', side_effect=['e', 'HELLO WORLD###', '27']):
+        mode, message, shift = enter_message() 
+        assert encrypt(message, shift) == 'IFMMP XPSME###'
+
+def test_decrypt_with_input_mock():
+    with patch('builtins.input', side_effect=['d', 'KHOOR', '3']):
+        mode, message, shift = enter_message() 
+        assert decrypt(message, shift) == 'HELLO'
+
+    with patch('builtins.input', side_effect=['d', 'BTWQI', '5']):
+        mode, message, shift = enter_message() 
+        assert decrypt(message, shift) == 'WORLD'
+
+    with patch('builtins.input', side_effect=['d', 'LIPPS ASVPH', '4']):
+        mode, message, shift = enter_message() 
+        assert decrypt(message, shift) == 'HELLO WORLD'
+
+    with patch('builtins.input', side_effect=['d', 'IFMMP XPSME###', '27']):
+        mode, message, shift = enter_message() 
+        assert decrypt(message, shift) == 'HELLO WORLD###'
+
